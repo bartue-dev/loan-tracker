262ed203879a92f03e7187caa1c2a8b653b6274d
Bartue <[bartbatiancila28@gmail.com]>
2025-03-17 23:23:11 +0800

    Add the getAddPersonForm as get request. postAddPerson as post request and postDeletePerson as post request

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,6 +2,12 @@ const { Router } = require("express");
 const indexRouter = Router();
 const indexController = require("../controllers/indexController")
 
-indexRouter.get("/", indexController.getListOfPersons )
+indexRouter.get("/", indexController.getListOfPersons);
+
+indexRouter.get("/addPerson", indexController.getAddPersonForm);
+
+indexRouter.post("/addPerson", indexController.postAddPerson);
+
+indexRouter.post("/:id/delete", indexController.postDeletePerson);
 
 module.exports = indexRouter
