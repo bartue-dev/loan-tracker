9e7eecf5b13cd0db27ea67977f32a92f0f02387c
Bartue <[bartbatiancila28@gmail.com]>
2025-03-17 19:25:44 +0800

    Import indexRouter. Set the path of views. Add express.static() method to be able to use static files in public directory. Replace use for rendering the index router to the server

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 const express = require("express");
 const app = express();
+const indexRouter = require("./routes/indexRouter");
+const path = require("node:path");
 
+//connect ejs
+app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.get("/", );
+//connect static files
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+//index route
+app.use("/", indexRouter);
 
 
 const PORT = 3000;
