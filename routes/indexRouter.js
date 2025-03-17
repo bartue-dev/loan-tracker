e7178c3ecbd6cd622ab5e97f40bdf5a9f6067f87
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-17 19:28:47 +0800

    Replace the getAllName to getListOfPersons Controller naming

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,4 +2,6 @@ const { Router } = require("express");
 const indexRouter = Router();
 const indexController = require("../controllers/indexController")
 
-exports.indexRouter.get("/", indexController.getAllName )
+indexRouter.get("/", indexController.getListOfPersons )
+
+module.exports = indexRouter
