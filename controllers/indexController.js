dd16fe87919e44a76bd5323aae4c3e75ce38dc72
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-18 19:40:12 +0800

    Added a getEditForm controller to render the edit form to the page. Add postEditForm controller to have a post request and edit an specific data

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -5,11 +5,9 @@ const asyncHandler = require("express-async-handler");
 exports.getListOfPersons = asyncHandler(async (req, res) => {
   const listOfPersons = await db.getListOfPersons();
 
-  console.log(listOfPersons);
-  
   res.render("index", {
     title: "List of Persons",
-    listPersons: listOfPersons
+    listPersons: listOfPersons,
   });
 });
 
@@ -29,5 +27,26 @@ exports.postDeletePerson = asyncHandler(async (req, res) => {
   const { id } = req.params;
   await db.deletePerson(id);
 
-  res.redirect("/")
+  res.redirect("/");
+});
+
+exports.getEditForm = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const personDetails = await db.getEditForm(id);
+
+  console.log("personal details: ", personDetails);
+  
+
+  res.render("editPerson", {details: personDetails});
+});
+
+exports.postEditPerson = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { firstname, lastname, address, phone_number } = req.body;
+
+  console.log("req query: ", req.body);
+
+  await db.editPerson(id, { firstname, lastname, address, phone_number })
+  
+  res.redirect("/");
 });
