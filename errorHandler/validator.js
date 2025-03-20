a6f4f74f704956e2e528b180c007c3e135b6cd7f
Bartue <[bartbatiancila28@gmail.com]>
2025-03-20 23:55:02 +0800

    Added a comment to certain code

--- errorHandler/validator.js
+++ errorHandler/validator.js
@@ -5,9 +5,9 @@ const emptyError = "must not be empty";
 const phoneError = "must contain 11 digit.";
 
 const validateInputs = [
-  body("firstname").trim()
-    .notEmpty().withMessage(`First name ${emptyError}`)
-    .matches(/^[a-zA-Z\s]*$/).withMessage(`First name ${alphaErr}`),
+  body("firstname").trim() //should trim the input first b4 validating
+    .notEmpty().withMessage(`First name ${emptyError}`) // should not empty
+    .matches(/^[a-zA-Z\s]*$/).withMessage(`First name ${alphaErr}`), //only allows letters and spaces along
   body("lastname").trim()
     .isAlpha().withMessage(`Last name ${alphaErr}`)
     .notEmpty().withMessage(`Last name ${emptyError}`),
