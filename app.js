023b44c09a70eb69dce7f951752aa6e3971e5269
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-20 23:46:57 +0800

    Add a error hanlder middleware

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
-const indexRouter = require("./routes/indexRouter");
 const path = require("node:path");
+
+const indexRouter = require("./routes/indexRouter");
 
 //connect ejs
 app.set("views", path.join(__dirname, "views"));
@@ -17,6 +18,11 @@ app.use(express.static(assetsPath));
 //index route
 app.use("/", indexRouter);
 
+//error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Internal Server Error!"})
+});
 
 const PORT = 3000;
 app.listen(PORT, () => {
