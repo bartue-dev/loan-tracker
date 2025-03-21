a932fe0ea23d3affdc79d80368a113db04dbb808
Bartue <172117404+bartue-dev@users.noreply.github.com>
2025-03-21 22:44:37 +0800

    Add a amount validation to addForm. Chain validation to editForm

--- errorHandler/validator.js
+++ errorHandler/validator.js
@@ -4,7 +4,7 @@ const alphaErr = "must only contain letter.";
 const emptyError = "must not be empty";
 const phoneError = "must contain 11 digit.";
 
-const validateInputs = [
+const validateAddForm = [
   body("firstname").trim() //should trim the input first b4 validating
     .notEmpty().withMessage(`First name ${emptyError}`) // should not empty
     .matches(/^[a-zA-Z\s]*$/).withMessage(`First name ${alphaErr}`), //only allows letters and spaces along
@@ -16,6 +16,25 @@ const validateInputs = [
   body("phone_number").trim()
     .isLength({ min: 11, max: 11 }).withMessage(`Phone # ${phoneError}`)
     .notEmpty().withMessage(`Phone # ${emptyError}`),
+  body("amount").trim()
+    .notEmpty().withMessage(`Loan amount ${emptyError}`)
 ];
 
-module.exports = {validateInputs};
+const validateEditForm = [
+  body("firstname").trim()
+    .notEmpty().withMessage(`First name ${emptyError}`)
+    .matches(/^[a-zA-Z\s]*$/).withMessage(`First name ${alphaErr}`),
+  body("lastname").trim()
+    .notEmpty().withMessage(`Last name ${emptyError}`)
+    .isAlpha().withMessage(`Last name ${alphaErr}`),
+  body("address").trim()
+    .notEmpty().withMessage(`Address ${emptyError}`),
+  body("phone_number").trim()
+    .isLength({ min: 11, max: 11}).withMessage(`Phone # ${phoneError}`)
+    .notEmpty().withMessage(`Phone # ${emptyError}`)
+]
+
+module.exports = {
+  validateAddForm,
+  validateEditForm
+};
